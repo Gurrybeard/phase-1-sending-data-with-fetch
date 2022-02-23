1ee2c14ea41e27e4db15ec2a684940bd3dc7bafb
Benjamin <bboksner@gmail.com>
2022-02-23 00:05:10 -0500

    commit tree 0fc71f8e6cefde3cd3837c1f242a5eccaed17098

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName,email){
+    const data ={
+        "name":userName,
+        "email":email
+
+    }
+    const config={
+        method:"POST",
+        headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+        
+    }
+    return fetch("http://localhost:3000/users",config)
+         .then(response => response.json())
+         .then(body=>addContact(body.id))
+         .catch(error=>addContact(error.message))
+        }
+
+    function addContact(id) {
+        document.getElementsByTagName("body")[0].innerHTML=id;
+    }
+         
